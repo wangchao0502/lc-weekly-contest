7593d45f603903146c379e8ddc74123bc97ef60d
Chao Wang <georgewang2007@163.com>
2021-01-16 18:51:37 +0800

    feat: update comment

--- src/com/raphael/lc/p167/TwoSum.java
+++ src/com/raphael/lc/p167/TwoSum.java
@@ -14,6 +14,7 @@ class TwoSum {
      * 你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
      */
     public int[] twoSum(int[] numbers, int target) {
+        // 本题可以用双指针，left++，right--，复杂度O(n)
         int len = numbers.length;
 
         for (int i = 0; i < len - 1; i++) {
